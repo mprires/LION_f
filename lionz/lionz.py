6b643c3efbeb1fbc25679dee1ef5cbae1c5d1fa8
Lalith <Lalith.shiyamsundar@meduniwien.ac.at>
2023-09-01 12:55:41 +0200

    Initialize lion repository

---
+++ lionz/lionz.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+LIONZ: Lesion Segmentation Tool
+-------------------------------
+
+This module, `lionz.py`, serves as the main entry point for the LIONZ toolkit.
+It provides capabilities for tumor and lesion segmentation in PET/CT datasets.
+
+Notes
+-----
+.. note:: 
+   For a full understanding of the capabilities and functionalities of this module, 
+   refer to the individual function and class docstrings.
+
+Attributes
+----------
+__author__ : str
+    Module author(s).
+    
+__email__ : str
+    Contact email for module inquiries.
+
+__version__ : str
+    Current version of the module.
+
+Examples
+--------
+To use this module, you can either import it into another script or run it directly:
+
+.. code-block:: python
+
+    import lionz
+    # Use functions or classes
+
+or:
+
+.. code-block:: bash
+
+    $ python lionz.py
+
+See Also
+--------
+constants : Module containing constant values used throughout the toolkit.
+display : Module responsible for displaying information and graphics.
+image_processing : Module with functions and classes for image processing tasks.
+input_validation : Module that provides functionalities for validating user inputs.
+resources : Contains resource files and data necessary for the toolkit.
+download : Handles downloading of data, models, or other necessary resources.
+
+"""
+
+__author__ = "Lalith kumar shiyam sundar, Sebastian Gutschmayer, Manuel pires"
+__email__ = "lalith.shiyamsundar@meduniwien.ac.at, sebastian.gutschmayer@meduniwien.ac.at, manuel.pires@meduniwien.ac.at"
+__version__ = "0.1"
+
+
+# Imports for the module
+
+
+
