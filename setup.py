e73d778d5f3c4f48e8cfac2fb8bc59624dc6e931
Lalith <Lalith.shiyamsundar@meduniwien.ac.at>
2023-09-01 12:58:07 +0200

    Initialize lionz package

---
+++ setup.py
@@ -0,0 +1,67 @@
+from setuptools import setup, find_packages
+
+# Authors and their corresponding emails
+AUTHORS = [
+    ("Lalith kumar shiyam sundar", "lalith.shiyamsundar@meduniwien.ac.at"),
+    ("Sebastian Gutschmayer", "sebastian.gutschmayer@meduniwien.ac.at"),
+    ("Manuel pires", "manuel.pires@meduniwien.ac.at")
+]
+
+# Convert the authors to a formatted string
+authors_string = ", ".join([name for name, email in AUTHORS])
+emails_string = ", ".join([email for name, email in AUTHORS])
+
+setup(
+    name='lionz',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'nnunetv2',
+        'nibabel',
+        'halo',
+        'pandas',
+        'SimpleITK',
+        'pydicom',
+        'argparse',
+        'imageio',
+        'numpy',
+        'mpire',
+        'openpyxl',
+        'matplotlib',
+        'pyfiglet',
+        'natsort',
+        'pillow',
+        'colorama',
+        'dask',
+        'rich',
+        'dicom2nifti',
+        'emoji',
+        'opencv-python',
+        'dask[distributed]'
+    ],
+    entry_points={
+        'console_scripts': [
+            'lionz=lionz.main:main'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    description='A toolkit for precise segmentation of tumors in PET/CT scans.',
+    long_description='''LIONZ, an acronym for Lesion Segmentation, is a robust Python tool designed 
+                      to act as a central platform for extracting tumor and lesion information 
+                      from PET/CT datasets. Built with accuracy and performance in mind, LIONZ aims 
+                      to streamline medical imaging tasks and contribute to enhanced diagnosis.''',
+    keywords='lesion segmentation, tumors, PET/CT, medical imaging',
+    author=authors_string,
+    author_email=emails_string,
+    url='https://github.com/QIMP-Team/lionz',  # Add the repo URL or documentation URL
+    download_url='https://github.com/QIMP-Team/lionz/archive/v0.1.tar.gz',  # Link to the release tarball
+    project_urls={
+        "Bug Tracker": "https://github.com/QIMP-Team/lionz/issues",
+        "Documentation": "https://lionz.readthedocs.io/en/latest/",
+        "Source Code": "https://github.com/QIMP-Team/lionz",
+    }
+)
