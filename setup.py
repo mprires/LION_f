de83e9d9dcd50c78e73e7a8af9816b560ebdac64
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2024-03-15 19:58:03 +0100

    Update setup.py
    Signed-off-by: nutellaBear <48599863+LalithShiyam@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ emails_string = ", ".join([email for name, email in AUTHORS])
 
 setup(
     name='lionz',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     install_requires=[
         'nnunetv2',
